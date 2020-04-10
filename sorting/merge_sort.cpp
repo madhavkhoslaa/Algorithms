24826505a18aa6104bb3bd17cf8f1213d36a29e0
Madhav Khosla <madhav.khoslaa@gmail.com>
2020-04-10 06:07:37 +0530

    merge sort

--- sorting/merge_sort.cpp
+++ sorting/merge_sort.cpp
@@ -1,48 +1,50 @@
-#include<iostream>
+#include <iostream>
 using namespace std;
-
-void merge(int *a,int s,int e){
-    int mid = (s+e)/2;
-    int i = s;
-    int j = mid+1;
-    int k = s;
-    int temp[100];
-    while(i<=mid && j<=e){
-        if(a[i] < a[j]){
-            temp[k++] = a[i++];
+void merge(int* a, int start, int end){
+    int mid= (start+ end)/ 2;
+    int i= start;
+    int k= start;
+    int j= mid+ 1;
+    int temp[200];
+    while(i<= mid && j<= end){
+        if(a[i]>= a[j]){
+            temp[k++]= a[j++];
         }
-        else{
-            temp[k++] = a[j++];
+        if(a[i]<= a[j]){
+            temp[k++]= a[i++];
         }
     }
-    while(i<=mid){
-        temp[k++] = a[i++];
+    while(i<= mid){
+        temp[k++]= a[i++];
     }
-    while(j<=e){
-        temp[k++]  = a[j++];
+    while(j<= end){
+        temp[k++]= a[j++];
     }
-    for(int i=s;i<=e;i++){
-        a[i] = temp[i];
+    for(int i= start; i<= end; i++){
+        a[i]= temp[i];
     }
 }
-void mergeSort(int a[],int s,int e){
-    if(s< e){
-        int mid = (s+e)/2;
-        mergeSort(a,s,mid);
-        mergeSort(a,mid+1,e);
-        merge(a,s,e);
+void mergesort(int a[], int start, int end){
+    if(start< end){
+        int mid= (start+ end)/2;
+        mergesort(a, start, mid);
+        mergesort(a, mid+ 1, end);
+        merge(a, start, end);
     }
 }
+int main() {
+    int n;
+    cout<<"Enter the length of the array"<<endl;
+    cin>>n;
+    int a[n];
+    cout<<"Enter Array elements "<<endl;
+    for(int i= 0; i<n; i++){
+        cin>>a[i];
+    }
+    mergesort(a, 0, n- 1);
+    for(int i= 0; i<n; i++){
+        cout<<a[i]<<",";
+    }
+    return 0;
 
-int main(){
-	int a[100];
-	int n;
-	cin>>n;
-	for(int i=0;i<n;i++){
-		cin>>a[i];
-	}
-	mergeSort(a,0,n-1);
-	for(int i=0;i<n;i++){
-	    cout<<a[i]<<" , ";
-	}
 }
