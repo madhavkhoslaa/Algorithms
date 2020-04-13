861581f61cd6072717027eb13eb6774066f65e78
Madhav Khosla <madhav.khoslaa@gmail.com>
2020-04-13 09:50:14 +0530

    prime number gen

---
+++ Misc/Eratosthenes_Sieve.cpp
@@ -0,0 +1,38 @@
+#include <iostream>
+#include <cmath>
+
+using namespace std;
+
+void primeSieve(int* plist, int n){
+    for(int i= 3; i< n; i++){
+            plist[i]= 1; 
+    }
+    for(int i= 3; i<n; i++){
+        if(i%2== 0)
+            plist[i]= 0;
+    }
+    for(long i= 3; i< n; i+= 2){
+        if(plist[i]== 1){
+            for(long j= i* i; j< n; j= j+ i)
+                plist[j]= 0;
+            }
+        }
+    plist[2]= 1;
+    plist[1]= plist[0]= 0;
+ 
+    }
+
+
+int main(){
+    int N;
+    cout<<"Enter the number";
+    cin>>N;
+    cout<<"Prime Numbers"<<endl;
+    int plist[N]= {1};
+    primeSieve(plist, N);
+    for(int i= 0; i< N; i++){
+        if(plist[i]== 1)
+            cout<<i<<endl;
+    }
+    return 0;
+}
